9afee55df933cce05d0db0f039b2df22b30205f6
codingniket <niketbachhawat7@gmail.com>
2025-11-25 12:50:22 +0530

    Luch

--- 25-11-2025/List_Baisc.py
+++ 25-11-2025/List_Baisc.py
@@ -16,3 +16,41 @@ l.sort(reverse=True)
 print(l)
 
 print([n*n for n in l]) #list comprehension
+
+
+fruit = ("Apple","Mango","Chawal")
+
+print(fruit)
+print(fruit[0])
+
+print(fruit.count("M"))
+print(fruit.index("Mango"))
+
+print(fruit[1:3])
+print(fruit[1:])
+print(fruit[:3])
+print(fruit[::-1])
+
+nums=[10,20,30]
+a,b,c = nums
+
+print(a)
+print(b)
+print(c)
+
+
+
+fruit = ("Apple","Mango","Chawal",("Lets","Go"))
+print(fruit[3][1])
+
+
+numbers=(1,34,5,0,243)
+s = 1000
+l=-1
+for x in numbers:
+    if x < s:
+        s=x
+    if x > l:
+        l=x
+print("Small Number",s,"Large Number",l)
+
