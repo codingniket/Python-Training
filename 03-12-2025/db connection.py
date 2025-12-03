f7b861f93ce9546d5b1bd396d16355b39fb2d665
codingniket <niketbachhawat7@gmail.com>
2025-12-03 17:13:27 +0530

    Ended of Day 8

--- 03-12-2025/db connection.py
+++ 03-12-2025/db connection.py
@@ -14,17 +14,42 @@ conn = pymysql.connect(
     host='localhost',
     user='root',
     password='root123',
-    database='company_db'
+    database='retail_db'
 )
 
 cursor = conn.cursor()
+# Read
+cursor.execute("SELECT p.product_name, o.order_date FROM products p INNER JOIN orders o ON o.product_id = p.product_id WHERE p.category = 'Groceries' ORDER BY o.order_date ASC")
 
-cursor.execute("select * from employees")
+#Insert
 
-rows = cursor.fetchall()
+cursor.execute("""
+    INSERT INTO products (product_id, product_name, category, price)
+    VALUES (101, 'Milk', 'Groceries', 45.00)
+""")
 
-for row in rows:
-    print(row)
+# Update product price
+cursor.execute("""
+    UPDATE products
+    SET price = 50.00
+    WHERE product_id = 101
+""")
+#DELETE
+cursor.execute("""
+    DELETE FROM products
+    WHERE product_id = 101
+""")
 
+
+
+cursor.execute("SHOW TABLES")
+tables = cursor.fetchall()
+
+for table in tables:
+    print(table[0])
+
+
+
+conn.commit()
 cursor.close()
 conn.close()
